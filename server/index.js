d83a2f128edf9611b317f441085c617742d87798
Umer Zafar <umerzafar4598@gmail.com>
2026-02-12 19:07:26 +0500

    CORS FIXES

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ const app = express();
 const PORT = process.env.PORT;
 const PgSession = connectPgSimple(session);
 
+const allowedOrigins = [process.env.CLIENT_URL, 'http://localhost:5173'];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,10 +25,15 @@ app.set('trust proxy', 1);
 
 app.use(
     cors({
-        origin: process.env.CLIENT_URL,
+        origin: function (origin, callback) {
+            // allow requests with no origin (like mobile apps or curl requests)
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                return callback(new Error('CORS policy violation'), false);
+            }
+            return callback(null, true);
+        },
         credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization']
     })
 );
 
